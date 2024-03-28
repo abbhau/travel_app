7f792046adc0ca4f71010715504bbb462be7a76e
Ashish <ashishbaraskar33@gmail.com>
2024-03-28 23:33:36 +0530

    UserSerializer with validations added

--- travel_app/auth_app/serializers.py
+++ travel_app/auth_app/serializers.py
@@ -5,10 +5,18 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     ''' Serializer for serialize the data of User and vice versa '''
-    
+
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['id','username','password','email']
+        fields = ['id','username','password','confirm_password','email']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError("Password and confirm_password not match")
+        return attrs
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')
         return User.objects.create_user(**validated_data)
