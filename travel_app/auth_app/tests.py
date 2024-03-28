8e43370e361c6ec915c4f932fe933619f81bf79d
Ashish <ashishbaraskar33@gmail.com>
2024-03-29 01:08:12 +0530

    Api endpoint testcase for user creation  created

--- travel_app/auth_app/tests.py
+++ travel_app/auth_app/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework import status
 
 # Create your tests here.
 class UserSerializerTestCase(TestCase):
@@ -16,14 +19,44 @@ class UserSerializerTestCase(TestCase):
         self.assertTrue(serializer.is_valid())
 
     def test_invalid_serialize_data(self):
-        ''' Test for invalid data or no data passed to the User serializer '''
+        ''' Test if no data passed to the User serializer '''
 
         obj = {
             "username": "ashish3",
             "password": "ganesh123",
-            "confirm_password": "ganesh123",
             "email": "as3@gmail.com" 
             }
         serializer = UserSerializer(data=obj) 
         self.assertFalse(serializer.is_valid())
+
+    def test_user_serializer_password(self):
+        '''tset for password and confirm password '''
+
+        obj =  {
+                "username": "ashish3",
+                "password": "ganesh123",
+                "confirm_password": "ganesh123",
+                "email": "as3@gmail.com" 
+                }
+        serializer = UserSerializer(data=obj)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['password'], serializer.validated_data['confirm_password'])
+
+
+class UserAPITestCase(TestCase):
+    '''test case for user create in User model'''
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='ashish7', password='password',
+                                    email='as3@gmail.com')
+
+    def test_create_user(self):
+        url = '/user/create/'
+        data = {'username': 'ashish9', 'password': 'password', 'email':"as3@gmail.com",
+                'confirm_password':'password'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+
 
